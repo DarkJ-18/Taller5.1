6f02f84e9529a715e4ddf7a36ac1d108fc40ac4f
Juan <j.juancamilojurado@gmail.com>
2019-09-14 00:56:50 -0500

    Initial commit

---
+++ Taller5.1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Taller5._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtnumeros = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrar.Location = new System.Drawing.Point(230, 99);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(69, 25);
+            this.btnmostrar.TabIndex = 0;
+            this.btnmostrar.Text = "Mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.Btnmostrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese cuantos numeros desea ver";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 173);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtnumeros
+            // 
+            this.txtnumeros.Location = new System.Drawing.Point(230, 54);
+            this.txtnumeros.Name = "txtnumeros";
+            this.txtnumeros.Size = new System.Drawing.Size(69, 20);
+            this.txtnumeros.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(38, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Suma de numeros impares.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 317);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtnumeros);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnmostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtnumeros;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Taller5.1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taller5._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btnmostrar_Click(object sender, EventArgs e)
+        {
+
+            int i, impar = 0;
+            for (i = 0; i <= Convert.ToInt16(txtnumeros.Text); i++)
+            {
+                if (i % 2 != 0)
+                {
+                    impar += i;
+
+                    listBox1.Items.Add(i.ToString());
+
+                }
+
+            }
+
+            MessageBox.Show("El resultado es: " + impar , "Numeros impares", MessageBoxButtons.OK);
+
+        }
+    }
+}
